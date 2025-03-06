c9497d5638a521d38593fbfc3a77b88dd4703d43
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-06 22:44:21 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import Chatbot from './Chatbot';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Harokopeio University ISCA Chatbot</h1>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
 
